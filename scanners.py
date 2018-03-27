c01ab75f0af6312d8b5c2abf53601c22f4509648
Kyle morishige <35090519+Morishigekyle@users.noreply.github.com>
2018-03-26 23:04:05 -1000

    Almost completed

--- scanners.py
+++ scanners.py
@@ -4,9 +4,9 @@ import re
 filePath = "./ssh.log.txt"
 fd = open(filePath, 'r')
 
-with fd as reader :
-    for line in reader :
-        print( line )
+# with fd as reader :
+#     for line in reader :
+#         # print( line )
 
 
 
@@ -34,9 +34,44 @@ with fd as reader :
 num_lines = sum(1 for line in open ("ssh.log.txt"))
 # print(num_lines)
 ScanAttempts = str(num_lines)
+f = open("ssh.log.txt","r")
+lines = f.readlines()
+# print(lines)
+for line in lines:
+	col = line.split("\t")
+	# print(col[2])
+	hostip = []
+	hostip.insert(0,col[2])
+	# allip = []
+	# [allip.extend(x) for x in hostip]
+	# for x in allip:
+	# 	print(x)
+	hosted = ''.join(str(e) for e in hostip)
+	# print(col[2])
+	# hostip = print(col[2])
+	# print(hostip)
+	hostip1 = []
+	hostip1.insert(0,col[4])
+	# print(hostip1)
+	hosted1 = ''.join(str(e) for e in hostip1)
+	# print(hosted1)
 
 f = open("scanners_found.txt", "w+")
-f.write("[Scan Attempts]" + " " + ScanAttempts)
-f.close()
+f.write("[Scan Attempts]" + " " + ScanAttempts + "\n")
+f.write("\n[Scan Origin Hosts]" + "\n " + hosted + "\n. . . . . . ." + "\n?\n")
+f.write("\n[Scan Destination Hosts]" + "\n " + hosted1 + "\n. . . . . . ." + "\n?\n")
 
-f = open("scanners_found.txt", "r")
+
+# f = open("scanners_found.txt", "r")
+
+
+
+
+	
+	# print(hostip)	
+	# uniqip = set(map(tuple, hostip))
+	# print(uniqip)
+	# uniqip = [list(t) for t in set(map(tuple, hostip))]
+	# print(uniqip)
+
+
